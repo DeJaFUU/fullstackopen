f679db2c9a9cab9d929418da9dc3b5a38a640a51
Frank <frankwoord@live.nl>
2022-02-02 16:54:31 +0100

    This was not delayed

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -18,11 +18,20 @@ function App() {
 
   const filteredCountries = countries.filter(country => country.name.common.toLowerCase().includes(newName.toLowerCase()))
 
+ {/*
+fetch(`http://api.openweathermap.org/data/2.5/weather?q=${filteredCountries[0].capital}&appid=redacted`)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+      })
+      */}
+
   const Countries = () => {
     if (filteredCountries.length > 10) {
       return <p>Too many matches, specify another filter</p>
     } else if (filteredCountries.length === 1) {
       return (
+        
         <div>
       <h1>{filteredCountries[0].name.common}</h1>
       <p>Capital: {filteredCountries[0].capital}
@@ -33,10 +42,13 @@ function App() {
      </ul> 
      <br />
       <img src={filteredCountries[0].flags.png} alt={filteredCountries[0].name.common} />
+      <h1>Weather in {filteredCountries[0].capital}</h1>
+      <p>Temprature: </p>
+      <p>Wind: </p>
       </div>
       )
     } else if (filteredCountries.length > 1) {
-      return <ul>{filteredCountries.map(country => <li key={country.name.common}>{country.name.common}</li>)}</ul>
+      return <ul>{filteredCountries.map(country => <li key={country.name.common}>{country.name.common} <button onClick={() => setNewName(country.name.common)}>show</button></li>)}</ul>
     } else {
       return <p>No matches</p>
   }
